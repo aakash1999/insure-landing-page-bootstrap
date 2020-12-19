f4d38fa92519b9f59b2118b917e698b700d18806
Aakash Kumar Nanda <aakash.nanda99@gmail.com>
2020-12-19 12:31:48 +0530

    Add files via upload

---
+++ insure-landing-page-aakash/js/main.js
@@ -0,0 +1,7 @@
+// Change mobile hamburger icon
+$(function() {
+  $("#nav-btn").click(function() {
+    $(".hamburger-open").toggleClass("hide");
+    $(".hamburger-close").toggleClass("hide");
+  });
+});
